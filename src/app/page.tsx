ef3e3c5f784903c3b00126e59cf1459ce44286f4
NeoEx <draja.acc@gmail.com>
2025-03-29 10:09:10 -0300

    feat: glassCard component done, applyed containerPage and glassCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useDispatch } from 'react-redux';
 import { Button } from "@/components/ui/button"
+import GlassCard from "@/components/cards/GlassCard";
+import ContainerPage from "@/components/containers/ContainerPage";
 
 
 
@@ -10,7 +12,7 @@ const Home = () => {
   const router = useRouter();
 
   return (
-      <section className="grid sm:grid-cols-2 grid-cols-1 items-start justify-items-center h-screen p-8 gap-16 sm:p-20 bg-[image:var(--color-background-grad)] relative w-full overflow-hidden z-0">
+      <ContainerPage className="grid sm:grid-cols-2 grid-cols-1 items-start justify-items-center pt-20 bg-[image:var(--color-background-grad)] relative z-0">
       <Image
         src="/bgVetorizado2.png"
         alt="Background"
@@ -19,10 +21,40 @@ const Home = () => {
         loading="eager"  
         className="sm:object-contain sm:object-right-top absolute h-screen w-full object-cover object-[40%]" 
       />    
-        <div 
+        {/* <div 
         className="flex flex-col items-center justify-center space-y-8 backdrop-blur-[10px] p-5 rounded-3xl mt-8 dark:bg-[#f0ffff94] shadow [--tw-shadow:var(--shadow)]
-">
-          <h1 className="text-4xl font-bold text-center text-black">
+"> */}
+          <GlassCard 
+            title="Welcome to Your Dashboard Creator" 
+            description="" 
+            content="Build, customize, and organize your dashboards and KPIs however you want. Choose layouts, define metrics, and create different views for each objective — all in one place." 
+            footer="Signin or Register to Start to Use."
+            cardClass="max-w-[90%]"
+            headerClass=""
+            titleClass=""
+            descriptionClass=""
+            contentClass=""
+            footerClass=""           
+          >
+            <div className="flex!important w-full justify-evenly!impotant">
+              <Button
+                variant="outline"
+                className="px-4 py-2 text-white bg-[#2c7aaf] rounded-md min-w-40"
+              >
+                Signin
+              </Button>
+              <Button 
+                variant="outline"
+                className="px-4 py-2 text-white bg-[#2c7aaf] rounded-md min-w-40"
+              >
+                Register
+              </Button>
+            </div>
+          </GlassCard>
+
+
+
+          {/* <h1 className="text-4xl font-bold text-center text-black">
             Welcome to Your Dashboard Creator
           </h1>
           <span>
@@ -45,9 +77,9 @@ const Home = () => {
             >
               Register
             </Button>
-          </div>
-        </div>
-      </section>
+          </div> */}
+        {/* </div> */}
+      </ContainerPage>
   );
 }
 
---
+++ src/components/cards/GlassCard.tsx
@@ -0,0 +1,42 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import React, { ReactNode } from "react";
+
+interface GlassCardProps {
+  title: string;
+  description: string;
+  content: string;
+  footer?: string;
+  children?: React.ReactNode;
+  cardClass?: string;
+  headerClass?: string;
+  titleClass?: string;
+  descriptionClass?: string;
+  contentClass?: string;
+  footerClass?: string;
+}
+
+
+const GlassCard: React.FC<GlassCardProps> = ({title, description, content, footer, children, cardClass, headerClass, titleClass, descriptionClass, contentClass, footerClass}) => {
+  return (
+    <Card className={`mx-auto p-2 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg shadow-[inset_0px_0px_20px_2px_#ffffff] ${cardClass}`}>
+      <CardHeader className={`flex flex-col items-center text-center p-0 gap-0 ${headerClass}`}>
+        <CardTitle className={`text-2xl font-bold text-primary ${titleClass}`}>{title}</CardTitle>
+        <CardDescription className={`${descriptionClass}`} >{description}</CardDescription>
+      </CardHeader>
+      <CardContent className={`px-2 text-justify italic text-secondary ${contentClass}`}>{content}</CardContent>
+      <CardFooter className={`px-2 grid justify-items-center items-center ${footerClass}`}>
+        {footer}
+        {children}
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default GlassCard;
